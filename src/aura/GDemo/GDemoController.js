c685e89932bc7bae3e45ca3c6bae4e7341c252d1
Wilson Miao Liu <lockleeliumiaowilson@gmail.com>
2018-08-04 15:36:14 +1000

    show parameters in Schema Explorer

--- src/aura/GDemo/GDemoController.js
+++ src/aura/GDemo/GDemoController.js
@@ -14,25 +14,9 @@
     },
 
     onClickField: function(cmp, event, helper) {
-        var index = Number(event.target.className);
-        var fields = cmp.get("v.fields");
-        var field = fields[index];
-        if(field) {
-            var fieldType = field.type;
-            var pos = 0;
-            while((pos = fieldType.indexOf('<')) >= 0) {
-                fieldType = fieldType.substring(pos + 1);
-            }
-            while((pos = fieldType.indexOf('[')) >= 0) {
-                fieldType = fieldType.substring(pos + 1);
-            }
-            if((pos = fieldType.indexOf('>')) >= 0) {
-                fieldType = fieldType.substring(0, pos);
-            }
-            if((pos = fieldType.indexOf(']')) >= 0) {
-                fieldType = fieldType.substring(0, pos);
-            }
-            helper.describe(cmp, fieldType);
+        var linkType = event.target.className;
+        if(linkType) {
+            helper.describe(cmp, linkType);
         }
     },
 
--- src/aura/GDemo/GDemoHelper.js
+++ src/aura/GDemo/GDemoHelper.js
@@ -1,68 +1,99 @@
 ({
-    describe: function(cmp, type) {
-        var action = cmp.get("c.describe");
-        action.setParams({ type : type });
+    getObjectType: function(type) {
+        var pos = 0;
+        while((pos = type.indexOf('<')) >= 0) {
+            type = type.substring(pos + 1);
+        }
+        while((pos = type.indexOf('[')) >= 0) {
+            type = type.substring(pos + 1);
+        }
+        if((pos = type.indexOf('>')) >= 0) {
+            type = type.substring(0, pos);
+        }
+        if((pos = type.indexOf(']')) >= 0) {
+            type = type.substring(0, pos);
+        }
 
-        action.setCallback(this, function(response) {
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                var data = response.getReturnValue();
-                if(!data) {
-                    console.error('Cound not describe');
-                }
-                var keys = Object.keys(data).sort();
-                var fields = keys.map(function(key) {
-                    var type = data[key];
-                    var hideLink = type === 'Boolean' ||
-                        type === 'Integer' ||
-                        type === 'Long' ||
-                        type === 'String' ||
-                        type === 'List<Boolean>' ||
-                        type === 'List<Integer>' ||
-                        type === 'List<Long>' ||
-                        type === 'List<String>';
+        if(type === 'Boolean' ||
+            type === 'Integer' ||
+            type === 'Long' ||
+            type === 'String') {
+            return '';
+        }
+        else {
+            return type;
+        }
+    },
+
+    describe: function(cmp, type) {
+        var self = this;
+        this.callServerAction(cmp, 'c.describe', { type : type }, function(data) {
+            if(!data) {
+                console.error('Cound not describe');
+            }
+            var keys = Object.keys(data).sort();
+            var fields = keys.map(function(key) {
+                var info = data[key];
+                if(typeof info === 'string') {
                     return {
                         name: key,
-                        type: type,
-                        hideLink: hideLink,
+                        type: {
+                            name: info,
+                            link: self.getObjectType(info),
+                        }
                     };
-                });
-
-                cmp.set('v.fields', fields);
-                cmp.set('v.currentTypeName', type);
-            }
-            else if (state === "INCOMPLETE") {
-            }
-            else if (state === "ERROR") {
-                var errors = response.getError();
-                if (errors) {
-                    if (errors[0] && errors[0].message) {
-                        console.error("Error message: " + errors[0].message);
-                    }
-                } else {
-                    console.error("Unknown error");
                 }
-            }
-        });
+                else {
+                    var params = info.params || {};
+                    var paramList = [];
+                    for(var paramName in params) {
+                        paramList.push({
+                            name: paramName,
+                            type: {
+                                name: params[paramName],
+                                link: self.getObjectType(params[paramName]),
+                            },
+                        });
+                    }
+                    return {
+                        name: key,
+                        params: paramList,
+                        type: {
+                            name: info.type,
+                            link: self.getObjectType(info.type),
+                        },
+                    };
+                }
+            });
 
-        $A.enqueueAction(action);
+            cmp.set('v.fields', fields);
+            cmp.set('v.currentTypeName', type);
+        });
     },
 
     execute: function(cmp) {
-        var action = cmp.get("c.execute");
-        action.setParams({ query : cmp.get('v.query') });
+        this.callServerAction(cmp, 'c.execute', { query : cmp.get('v.query') }, function(data) {
+            if(!data) {
+                console.error('Cound not execute');
+            }
+
+            var json = JSON.stringify(data, null, 4);
+
+            cmp.set('v.result', json);
+        });
+    },
+
+    callServerAction: function(cmp, actionName, params, callback) {
+        var action = cmp.get(actionName);
+        action.setParams(params);
 
         action.setCallback(this, function(response) {
             var state = response.getState();
             if (state === "SUCCESS") {
                 var data = response.getReturnValue();
-                if(!data) {
-                    console.error('Cound not execute');
+                if(callback) {
+                    callback(data);
                 }
-
-                var json = JSON.stringify(data, null, 4);
-
-                cmp.set('v.result', json);
             }
             else if (state === "INCOMPLETE") {
             }
