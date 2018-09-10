1873cc17a152b69c95956f94141de38ed4dc8d58
Wilson Miao Liu <lockleeliumiaowilson@gmail.com>
2018-09-10 15:42:29 +1000

    add root and back links

--- src/aura/GDemo/GDemoController.js
+++ src/aura/GDemo/GDemoController.js
@@ -16,12 +16,25 @@
     onClickField: function(cmp, event, helper) {
         var linkType = event.target.className;
         if(linkType) {
+            var lastTypes = cmp.get('v.lastTypes');
+            if(!lastTypes) {
+                lastTypes = [];
+            }
+            lastTypes.push(linkType);
+            cmp.set('v.lastTypes', lastTypes);
             helper.describe(cmp, linkType);
         }
     },
 
     onClickRoot: function(cmp, event, helper) {
         helper.describe(cmp);
+    },
+
+    onClickBack: function(cmp, event, helper) {
+        var lastTypes = cmp.get('v.lastTypes');
+        lastTypes = lastTypes.slice(0, lastTypes.length - 1);
+        cmp.set('v.lastTypes', lastTypes);
+        helper.describe(cmp, lastTypes[lastTypes.length - 1]);
     },
 
     onExecute: function(cmp, event, helper) {
